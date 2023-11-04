1a9c057edb5fede0ca04e7485fec3f8e1a7749d7
teacher <teacher@4PC0>
2023-11-04 20:32:18 +0300

    Logger singletone

---
+++ ConsoleApp5/FactoryMethod/Singleton/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.FactoryMethod.Singleton
+{
+    public class Logger
+    {
+        private static Logger instance;
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+                return instance;
+            }
+        }
+
+        public void LogInfo(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void LogWarning(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void LogError(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -2,15 +2,7 @@
 
 using ConsoleApp5.FactoryMethod.Singleton;
 
-Computer compuhter = new Computer();
-compuhter.Launch("Pentium");
-Console.WriteLine(compuhter.OS.Name);
-
-compuhter.OS = OS.GetInstance("Windows 11");
-Console.WriteLine(compuhter.OS.Name);
-
-var comp2 = new Computer();
-comp2.Launch("Windows xp");
-Console.WriteLine(comp2.OS.Name);
-
-OS os = new OS("sdfsfds");
+var logger = new Logger();
+Logger.Instance.LogInfo("info");
+Logger.Instance.LogError("error");
+Logger.Instance.LogWarning("warning");
